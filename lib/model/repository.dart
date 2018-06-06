7b2f8df3189a93cce0c361e4ae98bee53d3a8d7f
Ray Li <ray.li.verified@gmail.com>
2018-06-06 18:39:15 -0500

    Local Image Creation #3 Part 1
    *Create local images for games.

--- lib/model/repository.dart
+++ lib/model/repository.dart
@@ -1,7 +1,7 @@
 import 'game.dart';
 
 final List<Game> newGamesList = [
-  seaoftheives,
+  seaofthieves,
   detroitbh,
   godofwar,
   monsterhunterw,
@@ -20,149 +20,193 @@ final List<Game> popularGamesList = [
 final Game pvzgw2 = new Game(
   name: 'Plants vs. Zombies: Garden Warfare 2',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/pvzgw2.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/pvzgw2.jpg',
+  cover: '',
   description:
-      'The battle for suburbia grows to crazy new heights! In this hilarious, action-packed shooter, zombie leader Dr. Zomboss has strengthened his horde and rebuilt suburbia as a zombie utopia. But hope remains, because for the first time the plants are taking the offensive in an all-out attack to reclaim their turf. Play as the plants in the all-new 24-player Herbal Assault mode, or choose your side in 4-player co-op or Solo Play!',
+  'The battle for suburbia grows to crazy new heights! In this hilarious, action-packed shooter, zombie leader Dr. Zomboss has strengthened his horde and rebuilt suburbia as a zombie utopia. But hope remains, because for the first time the plants are taking the offensive in an all-out attack to reclaim their turf. Play as the plants in the all-new 24-player Herbal Assault mode, or choose your side in 4-player co-op or Solo Play!',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game codww2 = new Game(
   name: 'Call of Duty WWII',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/codww2.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/codww2.jpg',
+  cover: '',
   description:
-      'Step back in time with Call of Duty WWII. Land in Normandy on D-Day and battle across Europe through iconic locations in history’s most monumental war. Experience classic Call of Duty combat, the bonds of camaraderie, and the unforgiving nature of war. Go at it alone or grab your closest war buddies and take the battle online!',
+  'Step back in time with Call of Duty WWII. Land in Normandy on D-Day and battle across Europe through iconic locations in history’s most monumental war. Experience classic Call of Duty combat, the bonds of camaraderie, and the unforgiving nature of war. Go at it alone or grab your closest war buddies and take the battle online!',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game fc5 = new Game(
   name: 'Far Cry 5',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/fc5.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/fc5.jpg',
+  cover: '',
   description:
-      'Whether through coercion, starvation or forced baptisms, the cult will use every resource available to build a following in preparation of The Collapse. Committed to restoring freedom to Hope County, The Resistance has rallied together in order to stop the Father and his followers. And they’ll only succeed with your help.',
+  'Whether through coercion, starvation or forced baptisms, the cult will use every resource available to build a following in preparation of The Collapse. Committed to restoring freedom to Hope County, The Resistance has rallied together in order to stop the Father and his followers. And they’ll only succeed with your help.',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game riseoftr = new Game(
   name: 'Rise of the Tomb Raider ',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/riseoftr.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/riseoftr.jpg',
+  cover: '',
   description:
-      'In Rise of the Tomb Raider, Lara uncovers an ancient mystery that places her in the cross-hairs of a ruthless organization. Featuring epic, high-octane action moments set in the most beautiful hostile environments on earth, Rise of the Tomb Raider delivers a cinematic survival action adventure where you will join Lara Croft on her first tomb raiding expedition as she seeks to discover the secret of immortality.',
+  'In Rise of the Tomb Raider, Lara uncovers an ancient mystery that places her in the cross-hairs of a ruthless organization. Featuring epic, high-octane action moments set in the most beautiful hostile environments on earth, Rise of the Tomb Raider delivers a cinematic survival action adventure where you will join Lara Croft on her first tomb raiding expedition as she seeks to discover the secret of immortality.',
   platforms: [
     'XBOX ONE',
     'PS4',
   ],
+  rating: 3.3,
+  screenshots: [
+  ],
 );
 
 final Game detroitbh = new Game(
-  name: 'Detroit: Become Human',
-  box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/detroitbh.jpg',
-  cover:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/cover/detroitbh.jpg',
-  description:
-      'Travel to the near-future metropolis of Detroit – a city rejuvenated by an exciting technological development: androids. Witness your brave new world turn to chaos as you take on the role of Kara, a female android trying to find her own place in a turbulent social landscape.',
-  platforms: [
-    'PS4',
-  ],
-  rating: 4.0,
-  screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_2.png',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_4.jpg',
-  ]
+    name: 'Detroit: Become Human',
+    box:
+    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/detroitbh.jpg',
+    cover:
+    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/cover/detroitbh.jpg',
+    description:
+    'Travel to the near-future metropolis of Detroit – a city rejuvenated by an exciting technological development: androids. Witness your brave new world turn to chaos as you take on the role of Kara, a female android trying to find her own place in a turbulent social landscape.',
+    platforms: [
+      'PS4',
+    ],
+    rating: 4.0,
+    screenshots: [
+      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_1.jpg',
+      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_2.png',
+      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_3.jpg',
+      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_4.jpg',
+    ]
 );
 
 final Game shadowcolossus = new Game(
   name: 'Shadow of the Colossus',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/shadowcolossus.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/shadowcolossus.jpg',
+  cover: '',
   description:
-      'Armed with only a sword and a bow, journey through ancient lands to seek out gigantic beasts.',
+  'Armed with only a sword and a bow, journey through ancient lands to seek out gigantic beasts.',
   platforms: [
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game godofwar = new Game(
   name: 'God of War',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/godofwar.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/godofwar.jpg',
+  cover: '',
   description:
-      'Living as a man outside the shadow of the gods, Kratos must adapt to the unfamiliar Norse lands, unexpected threats, and a second chance at being a father.',
+  'Living as a man outside the shadow of the gods, Kratos must adapt to the unfamiliar Norse lands, unexpected threats, and a second chance at being a father.',
   platforms: [
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game monsterhunterw = new Game(
   name: 'Monster Hunter World',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/monsterhunterw.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/monsterhunterw.jpg',
+  cover: '',
   description:
-      'Monster Hunter: World puts you in a living, breathing ecosystem as a hunter that seeks and slays ferocious beasts in heart-pounding battles that unfold across the vast ever-changing terrain. Venture on quests alone or cooperatively with up to three hunters via online drop-in multiplayer.',
+  'Monster Hunter: World puts you in a living, breathing ecosystem as a hunter that seeks and slays ferocious beasts in heart-pounding battles that unfold across the vast ever-changing terrain. Venture on quests alone or cooperatively with up to three hunters via online drop-in multiplayer.',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game agentsofmayhem = new Game(
   name: 'Agents of Mayhem',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/agentsofmayhem.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/agentsofmayhem.jpg',
+  cover: '',
   description:
-      'In Agents of Mayhem, a nefarious plot is set in motion to build a world-altering device that will grant unrivaled power to the supervillain organization known as LEGION. Pick your squad of 3 highly skilled and iconic characters from a roster of 12 uniquely designed MAYHEM agents and get ready to save the world!',
+  'In Agents of Mayhem, a nefarious plot is set in motion to build a world-altering device that will grant unrivaled power to the supervillain organization known as LEGION. Pick your squad of 3 highly skilled and iconic characters from a roster of 12 uniquely designed MAYHEM agents and get ready to save the world!',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 
 final Game fallout4 = new Game(
   name: 'Fallout 4',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/fallout4.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/fallout4.jpg',
+  cover: '',
   description:
-      'As the sole survivor of Vault 111, you enter a world destroyed by nuclear war. Every second is a fight for survival, and every choice is yours. Only you can rebuild and determine the fate of the Wasteland. Welcome home.',
+  'As the sole survivor of Vault 111, you enter a world destroyed by nuclear war. Every second is a fight for survival, and every choice is yours. Only you can rebuild and determine the fate of the Wasteland. Welcome home.',
   platforms: [
     'XBOX ONE',
     'PS4',
+  ],
+  rating: 4.2,
+  screenshots: [
   ],
 );
 
 final Game mgsurvive = new Game(
   name: 'Metal Gear Survive',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/mgsurvive.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/mgsurvive.jpg',
+  cover: '',
   description:
-      'Create your own character and learn to survive. Scavenge resources, craft weapons, build a base camp and explore the unknown while developing the survival skills necessary in this hostile environment.',
+  'Create your own character and learn to survive. Scavenge resources, craft weapons, build a base camp and explore the unknown while developing the survival skills necessary in this hostile environment.',
   platforms: [
     'XBOX ONE',
     'PS4',
   ],
+  rating: 3.0,
+  screenshots: [
+  ],
 );
 
-final Game seaoftheives = new Game(
+final Game seaofthieves = new Game(
   name: 'Sea of Thieves',
   box:
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/seaoftheives.jpg',
+  'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/box/seaoftheives.jpg',
+  cover: '',
   description:
-      'Sea of Thieves offers the essential pirate experience, from sailing and fighting to exploring and looting—everything you need to live the pirate life and create all-new legends.',
+  'Sea of Thieves offers the essential pirate experience, from sailing and fighting to exploring and looting—everything you need to live the pirate life and create all-new legends.',
   platforms: [
     'XBOX ONE',
+  ],
+  rating: 3.3,
+  screenshots: [
   ],
 );
 //final Game  = new Game(
