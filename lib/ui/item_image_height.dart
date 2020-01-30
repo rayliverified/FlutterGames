e0506a39c5e50a607c30c183e1aa865e888bf8a1
Ray Li <ray.li.verified@gmail.com>
2020-01-29 20:09:50 -0600

    Update Fixed Image Widgets
    *Update Fixed Image Widgets to pass parameters through constructor.

--- lib/ui/item_image_height.dart
+++ lib/ui/item_image_height.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 
 class ImageHeightItem extends StatelessWidget {
   ImageHeightItem(this.buildContext, this.image, this.height,
-      {this.imageRatio = 1.50, this.onTap});
+      {this.imageRatio = 1.618,
+      this.onTap,
+      this.radius = 4,
+      this.elevation = 8});
   final BuildContext buildContext;
   final String image;
   final double height;
   final double imageRatio;
   final Function onTap;
+  final double radius;
+  final double elevation;
 
   @override
   Widget build(BuildContext context) {
     double width = imageRatio * height;
 
     return Material(
-      borderRadius: BorderRadius.circular(4),
+      borderRadius: BorderRadius.circular(radius),
       clipBehavior: Clip.hardEdge,
-      elevation: 8,
+      elevation: elevation,
       shadowColor: Color(0xCC000000),
       child: Ink.image(
         image: NetworkImage(image),
--- lib/ui/item_image_width.dart
+++ lib/ui/item_image_width.dart
@@ -1,25 +1,54 @@
 import 'package:flutter/material.dart';
 
-class ImageWidthItem extends StatelessWidget {
-  ImageWidthItem(this.buildContext, this.image, this.width,
-      {this.imageRatio = 1.50});
-  final BuildContext buildContext;
+class ImageFixedWidth extends StatelessWidget {
+  ImageFixedWidth(this.image, this.width,
+      {this.imageRatio = 1.618, this.placeholder = ""});
   final String image;
   final double width;
   final double imageRatio;
+  final String placeholder;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = imageRatio == null ? width : imageRatio * width;
+
+    return FadeInImage.assetNetwork(
+      image: image,
+      placeholder: placeholder,
+      width: width,
+      height: height,
+      fit: BoxFit.cover,
+      fadeOutDuration: Duration(milliseconds: 100),
+      fadeInDuration: Duration(milliseconds: 200),
+    );
+  }
+}
+
+class ImageFixedWidthRounded extends StatelessWidget {
+  ImageFixedWidthRounded(this.image, this.width,
+      {this.imageRatio = 1.618,
+      this.radius = 4,
+      this.elevation = 8,
+      this.placeholder = ""});
+  final String image;
+  final double width;
+  final double imageRatio;
+  final double radius;
+  final double elevation;
+  final String placeholder;
 
   @override
   Widget build(BuildContext context) {
     double height = imageRatio * width;
 
     return Material(
-      borderRadius: BorderRadius.circular(4),
+      borderRadius: BorderRadius.circular(radius),
       clipBehavior: Clip.hardEdge,
-      elevation: 8,
+      elevation: elevation,
       shadowColor: Color(0xCC000000),
       child: FadeInImage.assetNetwork(
         image: image,
-        placeholder: "assets/placeholder_cover.jpg",
+        placeholder: placeholder,
         width: width,
         height: height,
         fit: BoxFit.cover,
