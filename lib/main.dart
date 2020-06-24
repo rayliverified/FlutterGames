35ab30bd56d24be99038ec76989043728313e337
Ray Li <ray.li.verified@gmail.com>
2020-06-23 21:38:51 -0500

    Create Responsive Layout
    *Enable responsiveness with the Responsive Framework (https://github.com/Codelessly/ResponsiveFramework).

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_games/page/page_main.dart';
 import 'package:flutter_games/themes.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() => runApp(FlutterGames());
 
@@ -8,6 +9,19 @@ class FlutterGames extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, child),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(650, name: TABLET, scaleFactor: 1.25),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET, scaleFactor: 1.4),
+          ResponsiveBreakpoint.autoScale(1000, name: TABLET, scaleFactor: 1.8),
+        ],
+        background: Container(color: Color(0xFFF5F5F5)),
+      ),
       title: 'Flutter Games',
       theme: defaultTheme,
       home: MainPage(),
--- lib/page/page_coming_soon.dart
+++ lib/page/page_coming_soon.dart
@@ -13,24 +13,28 @@ class _ComingSoonPageState extends State<ComingSoonPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Icon(widget.icon, color: Colors.black26, size: 96),
-          Padding(padding: EdgeInsets.only(bottom: 36)),
-          Text('Coming soon!', style: Theme.of(context).textTheme.subhead),
-          Padding(padding: EdgeInsets.only(bottom: 8)),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 64),
-            child: Text("We're working hard on new features. Check back often!",
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .display1
-                    .apply(color: Colors.black54)),
-          ),
-        ],
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(widget.icon, color: Colors.black26, size: 96),
+            Padding(padding: EdgeInsets.only(bottom: 36)),
+            Text('Coming soon!', style: Theme.of(context).textTheme.subhead),
+            Padding(padding: EdgeInsets.only(bottom: 8)),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 64),
+              child: Text(
+                  "We're working hard on new features. Check back often!",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .display1
+                      .apply(color: Colors.black54)),
+            ),
+          ],
+        ),
       ),
     );
   }
